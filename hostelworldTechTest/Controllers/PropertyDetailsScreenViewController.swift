70d3d88c0c23ff4f8343d2475c70e4d81cd7b86b
aidan egan <eganjessie@hotmail.com>
2020-07-21 11:52:09 +0100

    adding scroll view to property details

--- hostelworldTechTest/Controllers/PropertyDetailsScreenViewController.swift
+++ hostelworldTechTest/Controllers/PropertyDetailsScreenViewController.swift
@@ -14,17 +14,15 @@ class PropertyDetailsScreenViewController: UIViewController {
     let propertyDetailsPepository = PropertyDetailsRepository()
     var propertyID: String?
 
+    
+    @IBOutlet weak var propertyDetailsStackView: UIStackView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var propertyImage: UIImageView!
+    @IBOutlet weak var propertyNameAddressLabel: UILabel!
 
-
-
-    @IBOutlet weak var propertyName: UILabel!
-    @IBOutlet weak var propertAddress: UILabel!
-    @IBOutlet weak var propertyCity: UILabel!
-    @IBOutlet weak var propertyCountry: UILabel!
-    @IBOutlet weak var propertyDescription: UILabel!
-    @IBOutlet weak var propertyDirection: UILabel!
-    @IBOutlet weak var propertyDetailsImage: UIImageView!
-
+    var propetyDescriptionLbl = UILabel()
+    var propertyDirectionsLbl = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -32,6 +30,7 @@ class PropertyDetailsScreenViewController: UIViewController {
             print("error")
             return
         }
+        
 
 
         propertyDetailsPepository.fetchProperyDetails(propertyID: propertyID, completion:  { (PropertyDetailsDataModel) in
@@ -43,25 +42,33 @@ class PropertyDetailsScreenViewController: UIViewController {
                 let propertyDetailsItem = PropertyDetailsItem(propertyName: propertyDetails.name, address1: propertyDetails.address1, address2: propertyDetails.address1, city: propertyDetails.city.name, country: propertyDetails.city.country, propertyDescription: propertyDetails.description, propertyDirections: propertyDetails.directions, imageUrl: imageURL!)
 
 
-                self.setPropertyDetails(propertyDetailsItem: propertyDetailsItem)
+                self.setPropertyDetailsUI(propertyDetailsItem: propertyDetailsItem)
             }
         })
     }
     
+    
+    
 
-    func setPropertyDetails(propertyDetailsItem: PropertyDetailsItem){
+    func setPropertyDetailsUI(propertyDetailsItem: PropertyDetailsItem){
 
         DispatchQueue.main.async {
+            
+            
+            self.propertyDirectionsLbl.text = "Directions: \(propertyDetailsItem.propertyDirections)"
+            self.propetyDescriptionLbl.text = "Description: \(propertyDetailsItem.propertyDescription)"
 
-            self.propertyDirection.lineBreakMode = .byWordWrapping
-            self.propertyDirection.numberOfLines = 0
-
-            self.propertyName.text = "\(propertyDetailsItem.propertyName)"
-            self.propertyCountry.text = propertyDetailsItem.country
-            self.propertyCity.text = propertyDetailsItem.city
-            self.propertAddress.text = propertyDetailsItem.address1
-            self.propertyDirection.text = propertyDetailsItem.propertyDirections
-            self.propertyDescription.text = propertyDetailsItem.propertyDescription
+            self.propertyDirectionsLbl.lineBreakMode = .byWordWrapping
+            self.propertyDirectionsLbl.numberOfLines = 13
+            
+            self.propetyDescriptionLbl.lineBreakMode = .byWordWrapping
+            self.propetyDescriptionLbl.numberOfLines = 10
+            
+            
+            self.propertyDetailsStackView.addArrangedSubview(self.propetyDescriptionLbl)
+            self.propertyDetailsStackView.addArrangedSubview(self.propertyDirectionsLbl)
+            
+            self.propertyNameAddressLabel.text = "\(propertyDetailsItem.propertyName) Hostel \n\(propertyDetailsItem.address1)\n\(propertyDetailsItem.city) \n\(propertyDetailsItem.country)"
 
         }
         
@@ -69,7 +76,7 @@ class PropertyDetailsScreenViewController: UIViewController {
             print(propertyDetailsItem.imageUrl)
             let data = try? Data(contentsOf: propertyDetailsItem.imageUrl) //make sure your image in this url does exist, otherwise unwrap in a if let check / try-catch
             DispatchQueue.main.async {
-                self.propertyDetailsImage.image = UIImage(data: data!)
+                self.propertyImage.image = UIImage(data: data!)
             }
         }
     }
